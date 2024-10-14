126d7bf6ed08bc98855d87f7d23a12d28fea9a4c
zulu <cesarzulu82@gmail.com>
2024-10-14 17:51:57 -0500

    cambios a la implementacion de Consolidados se pusieron mapas y sus funciones para agregar datos a los mapas y obtenerlos

--- src/Consolidado.cpp
+++ src/Consolidado.cpp
@@ -2,94 +2,22 @@
 
 Consolidado::Consolidado() = default;
 
-Consolidado::Consolidado(int idSexo, string sexo, int ano, int semestre, int inscritos, int admitidos, int primeraMatricula, int totalMatriculados, int graduados)
-    : idSexo(idSexo), sexo(sexo), ano(ano), semestre(semestre), inscritos(inscritos), admitidos(admitidos), matriculados(primeraMatricula), matriculadosPrimerSemestre(totalMatriculados), graduados(graduados) {}
-
-int Consolidado::getIdSexo()
+void Consolidado::agregarDatoIntConsolidado(string& clave,int dato)
 {
-    return idSexo;
+    datosIntConsolidados[clave] = dato;
 }
 
-void Consolidado::setIdSexo(int idSexo)
+void Consolidado::agregarDatoStringConsolidado(string& clave,string& dato)
 {
-    this->idSexo = idSexo;
+    datosStringConsolidados[clave] = dato;
 }
 
-string Consolidado::getSexo()
+int Consolidado::obtenerDatoIntConsolidado(string& clave)
 {
-    return sexo;
+    return datosIntConsolidados.at(clave);
 }
 
-void Consolidado::setSexo(string &sexo)
+string Consolidado::obtenerDatoStringConsolidado(string& clave)
 {
-    this->sexo = sexo;
-}
-
-int Consolidado::getAno()
-{
-    return ano;
-}
-
-void Consolidado::setAno(int ano)
-{
-    this->ano = ano;
-}
-
-int Consolidado::getSemestre()
-{
-    return semestre;
-}
-void Consolidado::setSemestre(int semestre)
-{
-    this->semestre = semestre;
-}
-
-int Consolidado::getInscritos()
-{
-    return inscritos;
-}
-
-void Consolidado::setInscritos(int inscritos)
-{
-    this->inscritos = inscritos;
-}
-
-int Consolidado::getAdmitidos()
-{
-    return admitidos;
-}
-
-void Consolidado::setAdmitidos(int admitidos)
-{
-    this->admitidos = admitidos;
-}
-
-int Consolidado::getMatriculados()
-{
-    return matriculados;
-}
-
-void Consolidado::setMatriculados(int matriculados)
-{
-    this->matriculados = matriculados;
-}
-
-int Consolidado::getMatriculadosPrimerSemestre()
-{
-    return matriculadosPrimerSemestre;
-}
-
-void Consolidado::setMatriculadosPrimerSemestre(int matriculadosPrimerSemestre)
-{
-    this->matriculadosPrimerSemestre = matriculadosPrimerSemestre;
-}
-
-int Consolidado::getGraduados()
-{
-    return graduados;
-}
-
-void Consolidado::setGraduados(int graduados)
-{
-    this->graduados = graduados;
+    return datosStringConsolidados.at(clave);
 }
