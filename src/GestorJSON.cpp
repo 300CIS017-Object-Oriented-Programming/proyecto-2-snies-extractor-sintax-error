91b747dbffef7689a8ca03f2d92f8669165e94bd
zulu <cesarzulu82@gmail.com>
2024-10-20 14:12:45 -0500

    implementacion de funciones de gestor json

--- src/GestorJSON.cpp
+++ src/GestorJSON.cpp
@@ -1,18 +1,11 @@
 #include "GestorJSON.h"
 // FIXME: falta toda la implementacion
-vector<int> GestorJSON::leerProgramas(string &ruta)
+
+bool GestorJSON::crearArchivo(string &ruta, map<int, ProgramaAcademico *> &mapadeProgramasAcademicos, vector<vector<string>>& matrizEtiquetas)
 {
 }
 
-vector<vector<string>> GestorJSON::leerArchivo(string &rutaBase, vector<string> &etiquetasColumnas, vector<int> &codigosSnies)
-{
-}
-
-bool GestorJSON::crearArchivo(string &ruta, map<int, ProgramaAcademico *> &mapadeProgramasAcademicos, vector<string> etiquetasColumnas)
-{
-}
-
-bool GestorJSON::crearArchivoBuscados(string &ruta, list<ProgramaAcademico *> &programasBuscados, vector<string> etiquetasColumnas)
+bool GestorJSON::crearArchivoBuscados(string &ruta, list<ProgramaAcademico *> &programasBuscados, vector<vector<string>>& matrizEtiquetas)
 {
 }
 
