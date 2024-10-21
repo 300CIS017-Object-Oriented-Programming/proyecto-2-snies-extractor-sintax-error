a66e0638c095919b5875a1edf9e93ffc081699e3
juanpnaranjo2006 <juanpnaranjo2006@javerianacali.edu.co>
2024-10-21 07:39:43 -0500

    MOD: Actualizado el .h del controlador al nuevo diseño

--- src/SNIESController.h
+++ src/SNIESController.h
@@ -17,16 +17,24 @@ class SNIESController
 
 private:
     map<int, ProgramaAcademico *> programasAcademicos;
-    GestorCsv gestorCsvObj;
+    vector<GestorArchivos*> gestoresArchivos;
     vector<vector<string>> matrizEtiquetas;
+    /*
+     Matriz Etiquetas[0] -> Etiquetas Atributos de Tipo String Programa Academico
+     Matriz Etiquetas[1] -> Etiquetas Atributos de Tipo Int Programa Academico
+     Matriz Etiquetas[2] -> Etiquetas Atributos de Tipo String Consolidado
+     Matriz Etiquetas[3] -> Etiquetas Atributos de Tipo Int Consolidado
+     Matriz Etiquetas[4] -> Sexos Disponibles
+     Matriz Etiquetas[5] -> Años a considerar (Se obtienen de los parametros de procesarDatosCsv)
+    */
 
 public:
     SNIESController() = default;
     SNIESController(string &, string &, string &, string &, string &, string &, string &);
     ~SNIESController();
-    void procesarDatosCsv(string &, string &);
-    void calcularDatosExtra(bool);
-    void buscarProgramas(bool, string &, int);
+    void procesarDatos(vector<string> anos);
+    void calcularDatosExtra(bool exportarArchivo);
+    void buscarProgramas(bool exportarArchivo, string& palabraClave, int idComparacion);
 };
 
 #endif
